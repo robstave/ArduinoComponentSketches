9c99949efb65c54c7033eb0b4aea39cd71345324
Rob Stave <robstave@yahoo.com>
2015-10-19 23:56:05 -0500

    Added ACS-85-0004

---
+++ ACS-85 ATTiny85 sketches/ACS-85-0004/ACS-85-0004.ino
@@ -0,0 +1,253 @@
+
+/**
+ * ACS-85-0004
+ * ATTiny85  Dual VCO Selectable Range
+ *
+ * Dual frequncy vco
+ * With an analog control to do ranges from LFO to High frequency
+ *
+ * 6 ranges HF1,HF2, MF1, MF2, LF1, LF2
+ *
+ * External pin 1       = Reset (not used)
+ * External pin 2 (PB3) = input 0 freq 1
+ * External pin 3 (PB4) = input 1 freq 2
+ * External pin 4       = GND
+ * External pin 5 (PB0) = output 0 output
+ * External pin 6 (PB1) = Output 1 output
+ * External pin 7 (PB2) = input 2 Select Range
+ * External pin 8       = Vcc
+ *
+ * V 1.0  -  First Version
+ *
+ * Note: This sketch has been written specifically for ATTINY85 and not Arduino uno
+ * Observations.
+ *
+ * I could not reach 4k freq with a long counter. I think the interrupt code was too
+ * much.  There might be a few things I can do to tweak this.   Instead, I just used a scalar bool 
+ * isLFO and really copied the code.  Yes, copied.  There is plenty of room on the chip for this
+ * code, so just unwind the code and duplicate it rather than add clock cycles to make it
+ * look tighter.
+ * 
+ * so if you add code to your interrupt, get out a frequncy counter and make sure
+ * that its hitting what you think it is.  If not, your interrupt is attempting to interrupt your interrupt.
+ * since it can not do that..it just gives you a lower frequncy.
+ * 
+ * If you absolutly have to have above 4k, look into using the timers directly to pwm
+ *
+ * Rob Stave (Rob the fiddler) ccby 2015
+ */
+
+
+
+//  ATTiny overview
+//                           +-\/-+
+//                    Reset 1|    |8  VCC
+//      (pin3) in 0 A3  PB3 2|    |7  PB2 (pin2) A1 Range
+//      (pin4) in 1 A2  PB4 3|    |6  PB1 (pin1) out 1
+//                      GND 4|    |5  PB0 (pin0) out 0
+//                           ------
+
+//Ranges for the pot.  Technically a small nuumber means a
+//shorter timer so low or high...whatever you want to call it.
+
+/**
+ * For the HF and Medium Frequncies, we set the OCR1A = 100
+ * So to figure out the frequencies, Divide you divisor by the number*2
+ *
+ * 8mhz/2/(99+1) = 40000
+ * divide by number*2 (because its a squarewave)
+ */
+
+// 6 -> 4khz
+// 80 -> 40000/80/2 =  250hz
+
+#define VCO1_H1_LOW 5
+#define VCO1_H1_HIGH 80
+#define VCO2_H1_LOW 5
+#define VCO2_H1_HIGH 80
+
+
+// 30 -> 666 hz
+// 120 - > 166 hz
+#define VCO1_H2_LOW 30
+#define VCO1_H2_HIGH 120
+#define VCO2_H2_LOW 30
+#define VCO2_H2_HIGH 120
+
+// 90 -> 222hz
+// 320 -> 62.5
+#define VCO1_M1_LOW 90
+#define VCO1_M1_HIGH 320
+#define VCO2_M1_LOW 90
+#define VCO2_M1_HIGH 320
+
+// 150 -> 133
+// 520 - > 38.5
+#define VCO1_M2_LOW 150
+#define VCO1_M2_HIGH 520
+#define VCO2_M2_LOW 150
+#define VCO2_M2_HIGH 520
+
+
+/**
+ * Note in the switch, we change it up with an extra
+ * nested loop of 50
+ *
+ * so
+ * 8mhz/2/100/50 = 800hz Interrupt freq
+ */
+ 
+//25 -> 800/2 = 16
+// 400  -> 1
+#define VCO1_L1_LOW 25
+#define VCO1_L1_HIGH 400
+#define VCO2_L1_LOW 25
+#define VCO2_L1_HIGH 400
+
+ 
+//100 -> 4
+//2000 -> .2
+#define VCO1_L2_LOW 100
+#define VCO1_L2_HIGH 2000
+#define VCO2_L2_LOW 100
+#define VCO2_L2_HIGH 2000
+
+
+
+//counters for the frequencies
+int oscFreq1 = 0;
+int oscCounter1 = 0;
+int oscFreq2 = 0;
+int oscCounter2 = 0;
+
+boolean isLFO = false;
+int lfoCounter = 0;
+
+int range = 0;  //from 0 to 5
+int loopCount = 0;
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+  DDRB = B00000011;  //set output bits
+
+  // initialize timer1
+  noInterrupts();           // disable all interrupts
+
+  TCCR1 = 0;                  //stop the timer
+  TCNT1 = 0;                  //zero the timer
+  //GTCCR = _BV(PSR1);          //reset the prescaler
+  OCR1A = 99;                //set the compare value
+  OCR1C = 99;
+  TIMSK = _BV(OCIE1A);        //interrupt on Compare Match A
+
+  TCCR1 = _BV(CTC1) | _BV(CS11); // Start timer, ctc mode, prescaler clk/2
+
+  interrupts();             // enable all interrupts
+
+}
+
+ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
+{
+
+  if (isLFO == true) {
+    //Count up and toggle portB bits
+    if (lfoCounter > 50) {
+      lfoCounter = 0;
+
+      //Count up and toggle portB bits
+      if (oscCounter1 > oscFreq1) {
+        oscCounter1 = 0;
+        PORTB ^= (_BV(PB0));
+      }
+      oscCounter1++;
+
+      if (oscCounter2 > oscFreq2) {
+        oscCounter2 = 0;
+        PORTB ^= (_BV(PB1));
+      }
+      oscCounter2++;
+
+    }
+    lfoCounter++;
+
+    return;
+  }
+
+  //Count up and toggle portB bits
+  if (oscCounter1 == 0) {
+    oscCounter1 = oscFreq1;
+    PORTB ^= (_BV(PB0));
+  }
+  oscCounter1--;
+
+  if (oscCounter2 == 0) {
+    oscCounter2 = oscFreq2;
+    PORTB ^= (_BV(PB1));
+  }
+  oscCounter2--;
+
+}
+
+void loop() {
+
+  //no need to monitor range that often
+  if (loopCount > 20) {
+    loopCount = 0;
+    int   range_t = analogRead(A1);
+    range = map(range_t, 0, 1023, 0,  5);
+  }
+  loopCount++;
+
+
+  int osc1_t = analogRead(A3);
+  int osc2_t = analogRead(A2);
+
+  switch (range) {
+    case 0:
+      isLFO = false;
+      oscFreq1 = map(osc1_t, 0, 1023, VCO1_H1_LOW,  VCO1_H1_HIGH);
+      oscFreq2 = map(osc2_t, 0, 1023, VCO2_H1_LOW,  VCO2_H1_HIGH);
+      break;
+
+    case 1:
+      isLFO = false;
+      oscFreq1 = map(osc1_t, 0, 1023, VCO1_H2_LOW,  VCO1_H2_HIGH);
+      oscFreq2 = map(osc2_t, 0, 1023, VCO2_H2_LOW,  VCO2_H2_HIGH);
+      break;
+
+    case 2:
+      isLFO = false;
+      oscFreq1 = map(osc1_t, 0, 1023, VCO1_M1_LOW,  VCO1_M1_HIGH);
+      oscFreq2 = map(osc2_t, 0, 1023, VCO2_M1_LOW,  VCO2_M1_HIGH);
+      break;
+
+    case 3:
+      isLFO = false;
+      oscFreq1 = map(osc1_t, 0, 1023, VCO1_M2_LOW,  VCO1_M2_HIGH);
+      oscFreq2 = map(osc2_t, 0, 1023, VCO2_M2_LOW,  VCO2_M2_HIGH);
+      break;
+
+    case 4:
+      isLFO = true;
+      oscFreq1 = map(osc1_t, 0, 1023, VCO1_L1_LOW,  VCO1_L1_HIGH);
+      oscFreq2 = map(osc2_t, 0, 1023, VCO2_L1_LOW,  VCO2_L1_HIGH);
+      break;
+
+    case 5:
+      isLFO = true;
+      oscFreq1 = map(osc1_t, 0, 1023, VCO1_L2_LOW,  VCO1_L2_HIGH);
+      oscFreq2 = map(osc2_t, 0, 1023, VCO2_L2_LOW,  VCO2_L2_HIGH);
+      break;
+
+    default:
+      //huh...well..skip it
+      break;
+  }
+
+
+
+
+
+}
---
+++ ACS-85 ATTiny85 sketches/ACS-85-0004/README.md
@@ -0,0 +1,33 @@
+# ACS-85-0004
+Two Tone with Selectable range
+==============
+
+## Overview:
+Perhaps a bit more utilitarian than most.  Two vcos with an analog pin to select the range from one of 6 ranges.  High Freq 1, High Freq 2, Medium Freq 1, Medium Freq 2 and Low Freq 1 and 2.
+
+
+Range    | Low   | High 
+--- | --- | ---
+HF1  |  250 Hz  |  4Khz
+HF2  |  166 Hz  |  666 Hz
+MF1  |  62.5 Hz  |  222 Hz
+MF2  |  38.5 Hz  |  133 Hz
+LF1  |  1 Hz  | 16 Hz
+LF2  |  .2 Hz  | 4 Hz
+
+You can change the values, but keep in mind the HF and MF counter is an INT.
+For the low frequencies, the code was placed in another loop.  Changing the int to a long did not work well and I could not get above 1.5Khz.  I think this is because the work in the interrupt handler for longs takes way more clock cycles.
+I also ended up copying the counter code for the LFO branch rather than add complexity (and clock cycles) making a handler that looks tight.
+
+
+## Pinout:
+[![Pinout](https://github.com/robstave/ArduinoComponentSketches/blob/master/ACS-85%20ATTiny85%20sketches/ACS-85-0004/images/ACS-85-0004.png)] (https://github.com/robstave/ArduinoComponentSketches/blob/master/ACS-85%20ATTiny85%20sketches/ACS-85-0004/images)
+
+## Equivalent:
+[![Equivalent](https://github.com/robstave/ArduinoComponentSketches/blob/master/ACS-85%20ATTiny85%20sketches/ACS-85-0004/images/ACS-85-0004-overview.png)] (https://github.com/robstave/ArduinoComponentSketches)
+
+Two 40106 oscillators with selectable rc constants
+
+
+## Examples:
+ 
