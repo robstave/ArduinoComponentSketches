86fefde08ba88877e293596e37738542d07fd723
unknown <robstave@yahoo.com>
2018-02-19 20:34:46 -0600

    Added volatile

--- ACS-85 ATTiny85 sketches/ACS-85-0005/ACS-85-0005.ino
+++ ACS-85 ATTiny85 sketches/ACS-85-0005/ACS-85-0005.ino
@@ -1,42 +1,43 @@
 
 /**
- * ACS-85-0004
- * ATTiny85  Tri VCO With Detune
- *
- *
- * External pin 1       = Reset (not used)
- * External pin 2 (PB3) = input 0 freq 1
- * External pin 3 (PB4) = input 1 freq 2
- * External pin 4       = GND
- * External pin 5 (PB0) = output 0 output
- * External pin 6 (PB1) = Output 1 output
- * External pin 7 (PB2) = Output 2 output
- * External pin 8       = Vcc
- *
- *
- * Took several iterations to find the sweet spot.
- * The timer is not prescaled, so the interrupt is
- * really about as long as the value you set for OCR0A.
- * 
- * By this point, you can see that I get my frequencies with counters.
- * in this case, 8mhz/(79+1)/50/2 = 1khz
- * 
- * To get more resolution, you need a faster interrupt, but if its too fast, then
- * the size of the interrupt is too big and your top frequency actually goes down.
- * 
- * with int counters, I could not get above 750hz.  But I swapped the ints for bytes
- * and it saved several clocks
- * 
- * 
- * V 1.0  -  First Version
- * 
- * 
- *
- * Note: This sketch has been written specifically for ATTINY85 and not Arduino uno
- * Observations.
- *
- * Rob Stave (Rob the fiddler) ccby 2015
- */
+   ACS-85-0004
+   ATTiny85  Tri VCO With Detune
+
+
+   External pin 1       = Reset (not used)
+   External pin 2 (PB3) = input 0 freq 1
+   External pin 3 (PB4) = input 1 freq 2
+   External pin 4       = GND
+   External pin 5 (PB0) = output 0 output
+   External pin 6 (PB1) = Output 1 output
+   External pin 7 (PB2) = Output 2 output
+   External pin 8       = Vcc
+
+
+   Took several iterations to find the sweet spot.
+   The timer is not prescaled, so the interrupt is
+   really about as long as the value you set for OCR0A.
+
+   By this point, you can see that I get my frequencies with counters.
+   in this case, 8mhz/(79+1)/50/2 = 1khz
+
+   To get more resolution, you need a faster interrupt, but if its too fast, then
+   the size of the interrupt is too big and your top frequency actually goes down.
+
+   with int counters, I could not get above 750hz.  But I swapped the ints for bytes
+   and it saved several clocks
+
+
+   V 1.0  -  First Version
+   v 1.1  -  Count down vs up
+
+
+
+   Note: This sketch has been written specifically for ATTINY85 and not Arduino uno
+   Observations.
+
+   Rob Stave (Rob the fiddler) ccby 2015
+*/
 
 //  ATTiny overview
 //                      +-\/-+
@@ -51,16 +52,16 @@
 // 120 - > 166 hz
 #define VCO1_HIGH 50
 #define VCO1_LOW 200
- 
+
 
 //counters for the frequencies
- volatile uint8_t oscFreq1 = 200;
- volatile uint8_t oscCounter1 = 0;
- volatile uint8_t oscFreq2 = 200;
- volatile uint8_t oscCounter2 = 0;
- volatile uint8_t oscFreq3 = 200;
- volatile uint8_t oscCounter3 = 0;
- 
+volatile uint8_t oscFreq1 = 200;
+volatile uint8_t oscCounter1 = 200;
+volatile uint16_t oscFreq2 = 200;
+volatile uint16_t oscCounter2 = 200;
+volatile uint16_t oscFreq3 = 200;
+volatile uint16_t oscCounter3 = 200;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
 
@@ -73,10 +74,8 @@ void setup() {
   TCCR0B = 0;
 
   TCCR0A |= (1 << WGM01); //Start timer 1 in CTC mode Table 11.5
-
   OCR0A = 79; //CTC Compare value
-
-  TCCR0B |= (1 << CS00); // Prescaler =8 Table 11.6
+  TCCR0B |= (1 << CS00); // Prescaler =0 Table 11.6
 
   // TCCR0A |=(1<<COM0A1); //Timer0 in toggle mode Table 11.2
   TIMSK |= (1 << OCIE0A); //Enable CTC interrupt see 13.3.6
@@ -87,31 +86,31 @@ ISR(TIMER0_COMPA_vect)          // timer compare interrupt service routine
 {
 
   //Count up and toggle portB bits
-  if (oscCounter1 > oscFreq1) {
-    oscCounter1 = 0;
+  if (oscCounter1 <= 0) {
+    oscCounter1 = oscFreq1;
     PORTB ^= (_BV(PB0));
   }
-  oscCounter1++;
+  oscCounter1--;
 
-  if (oscCounter2 > oscFreq2) {
-    oscCounter2 = 0;
+  if (oscCounter2 <= 0) {
+    oscCounter2 = oscFreq2;
     PORTB ^= (_BV(PB1));
   }
-  oscCounter2++;
+  oscCounter2--;
 
-  if (oscCounter3 > oscFreq3) {
-    oscCounter3 = 0;
+  if (oscCounter3 <= 0 ) {
+    oscCounter3 = oscFreq3;
     PORTB ^= (_BV(PB2));
   }
-  oscCounter3++;
-  
+  oscCounter3--;
+
 }
 
 int detuneCount = 0;
 
 void loop() {
 
-  //Read freq 
+  //Read freq
   int osc1_t = analogRead(A3);
   oscFreq1 = map(osc1_t, 0, 1023, VCO1_LOW,  VCO1_HIGH);
 
@@ -122,7 +121,7 @@ void loop() {
     int osc2_t = analogRead(A2);
     int detune  = map(osc2_t, 0, 1023, 0,  20);
     oscFreq2 = oscFreq1 + detune;
-     oscFreq3 = oscFreq2 + detune;
+    oscFreq3 = oscFreq2 + detune;
   }
   detuneCount++;
 
