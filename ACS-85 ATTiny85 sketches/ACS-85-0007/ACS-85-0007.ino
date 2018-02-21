1eddbdb7279b9155e70fab065bed3f56d5bbbad3
unknown <robstave@yahoo.com>
2018-02-21 01:29:09 -0600

    updates to 0007

--- ACS-85 ATTiny85 sketches/ACS-85-0007/ACS-85-0007.ino
+++ ACS-85 ATTiny85 sketches/ACS-85-0007/ACS-85-0007.ino
@@ -1,77 +1,75 @@
 
 /**
- * ACS-85-0006
- * ATTiny85  Square wave with variable frequency.
- * The period changes in this case using counters.
- * Each time the pin flips, the period is reduced or increased.
- * The side of the increase is masked by 3,4 or 5 bits. Since the
- * period increases, the frequency decreases.  Its much more of ramp sound
- * for PB2
- *
- *
- * so for
- * PB0, the period is n + (x mod 8)
- * PB1, the period is n + (x mod 16)
- * PB2, the period is n + (x mod 32)
- *
- *
- *
- * External pin 1       = Reset (not used)
- * External pin 2 (PB3) = input 0 freq 1
- * External pin 3 (PB4) = none
- * External pin 4       = GND
- * External pin 5 (PB0) = output 0 smaller variance
- * External pin 6 (PB1) = Output 1
- * External pin 7 (PB2) = Output 2 larger variance
- * External pin 8       = Vcc
- *
- *
- * V 1.0  -  First Version
- *
- * Note: This sketch has been written specifically for ATTINY85 and not Arduino uno.
- *   It might work, but you prob need to change Pins/ports
- *
- * Rob Stave (Rob the fiddler) ccby 2015
- */
+   ACS-85-0007
+   ATTiny85  Square wave with variable frequency.
+
+   The period changes in this case using counters.
+   Each time the pin flips, the period is reduced or increased.
+   The size of the increase is masked by 3,4 or 5 bits. Since the
+   period increases, the frequency decreases.  Its much more of rampy sound
+   for PB2
+
+
+   so for
+   PB0, the period is n + (x mod 8)
+   PB1, the period is n + (x mod 16)
+   PB2, the period is n + (x mod 32)
+
+
+
+   External pin 1       = Reset (not used)
+   External pin 2 (PB3) = input 0 freq 1
+   External pin 3 (PB4) = none
+   External pin 4       = GND
+   External pin 5 (PB0) = output 0 smaller variance
+   External pin 6 (PB1) = Output 1
+   External pin 7 (PB2) = Output 2 larger variance
+   External pin 8       = Vcc
+
+
+   V 1.0  -  First Version
+
+   Note: This sketch has been written specifically for ATTINY85 and not Arduino uno.
+     It might work, but you prob need to change Pins/ports
+
+   Rob Stave (Rob the fiddler) ccby 2015
+*/
 
 
 
 //  ATTiny overview
-//                           +-\/-+
-//                    Reset 1|    |8  VCC
-//      (pin3) in 0 A3  PB3 2|    |7  PB2 (pin2) out 2
-//      (pin4)    none  PB4 3|    |6  PB1 (pin1) out 1
-//                      GND 4|    |5  PB0 (pin0) out 0
-//                           ------
+//                      +-\/-+
+//               Reset 1|    |8  VCC
+// (pin3) in 0 A3  PB3 2|    |7  PB2 (pin2) out 2
+// (pin4)    none  PB4 3|    |6  PB1 (pin1) out 1
+//                 GND 4|    |5  PB0 (pin0) out 0
+//                      ------
 
 
-
-// 30 -> 666 hz
-// 120 - > 166 hz
-#define VCO1_HIGH 50
-#define VCO1_LOW 200
-
-
-
+/**
+ * Ranges for the counters.
+ * A low value means a faster countdown and thus a higher frequency
+ * 
+ * 40 => 1250hz
+ * 45 => 1,111 hz
+ * 215 - > 232 hz
+ * 222 => 225 hz
+ *  Note: the counter is a byte and the ramp is max 5 bits = 32...so the VCO1_LOW should max out at 223
+ */
+ 
+#define VCO1_HIGH 40
+#define VCO1_LOW 223
 
 //counters for the frequencies
-volatile byte oscFreq1 = 200;
-volatile byte  oscCounter1 = 0;
-volatile byte nextClock1 = 200;
+volatile uint8_t oscFreq1 = VCO1_LOW;
 
-volatile byte  oscCounter2 = 0;
-volatile byte nextClock2 = 200;
+volatile uint8_t oscCounter1 = VCO1_LOW;
+volatile uint8_t oscCounter2 = VCO1_LOW;
+volatile uint8_t oscCounter3 = VCO1_LOW;
 
-
-volatile byte  oscCounter3 = 0;
-volatile byte nextClock3 = 200;
-
-
-volatile byte counter1 = 1;
-volatile byte counter2 = 1;
-volatile byte counter3 = 1;
-
-
+volatile uint8_t counter1 = 0;
+volatile uint8_t counter2 = 0;
+volatile uint8_t counter3 = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -88,7 +86,7 @@ void setup() {
 
   OCR0A = 79; //CTC Compare value
 
-  TCCR0B |= (1 << CS00); // Prescaler =8 Table 11.6
+  TCCR0B |= (1 << CS00); // Prescaler =0 Table 11.6
 
   // TCCR0A |=(1<<COM0A1); //Timer0 in toggle mode Table 11.2
   TIMSK |= (1 << OCIE0A); //Enable CTC interrupt see 13.3.6
@@ -99,42 +97,32 @@ ISR(TIMER0_COMPA_vect)          // timer compare interrupt service routine
 {
 
   //Count up and toggle portB bits
-  if (oscCounter1 > nextClock1) {
-    oscCounter1 = 0;
+  if (oscCounter1 <= 0) {
     counter1++;
-    nextClock1 = oscFreq1 + ( counter1 & B00000111);
-
+    oscCounter1 = oscFreq1 + ( counter1 & B00000111);
     PORTB ^= (_BV(PB0));
   }
-  oscCounter1++;
+  oscCounter1--;
 
-  if (oscCounter2 > nextClock2) {
-    oscCounter2 = 0;
-
+  if (oscCounter2 <= 0) {
     counter2++;
-
-    nextClock2 = oscFreq1 + ( counter2 & B00001111);
+    oscCounter2 = oscFreq1 + ( counter2 & B00001111);
     PORTB ^= (_BV(PB1));
   }
-  oscCounter2++;
+  oscCounter2--;
 
-  if (oscCounter3 > nextClock3) {
-    oscCounter3 = 0;
-
+  if (oscCounter3 <= 0) {
     counter3++;
-    nextClock3 = oscFreq1 +  ( counter3 & B00011111);
+    oscCounter3 = oscFreq1 +  ( counter3 & B00011111);
     PORTB ^= (_BV(PB2));
   }
-  oscCounter3++;
+  oscCounter3--;
 
 }
-
 
 void loop() {
 
   //Read freq
   int osc1_t = analogRead(A3);
   oscFreq1 = map(osc1_t, 0, 1023, VCO1_LOW,  VCO1_HIGH);
-
-
 }
--- ACS-85 ATTiny85 sketches/ACS-85-0007/README.md
+++ ACS-85 ATTiny85 sketches/ACS-85-0007/README.md
@@ -1,8 +1,9 @@
-# ACS-85-0007
-Square VCO with freq ramping
+# ACS-85-0007 - Square VCO with freq ramping
+
 ==============
 
-## Overview:
+## Overview
+
 ATTiny85  Square wave with variable frequency.
 The period changes in this case using counters. Each time the pin flips, the period is increased. The size of the increase is masked by 3,4 or 5 bits. Since the period increases, the frequency decreases.  
 
@@ -14,16 +15,19 @@ For PB1, we double the size of the counter (3 bits to 4) so the lowest frequency
 Its much more of a dramatic ramp sound for PB2. More of a Raygun sound.
  
 
-## Pinout:
+## Pinout
+
 ![Pinout](https://github.com/robstave/ArduinoComponentSketches/blob/master/ACS-85%20ATTiny85%20sketches/ACS-85-0007/images/ACS-85-0007.png)
 
-## Equivalent:
-
-## Examples:
+## Examples
 
 https://soundcloud.com/user-692410397/85-0007a
 
 Tests at each pin from PB0 (fat square) to PB2 (Raygun)
+
 ## Improvements
 
-There is an open pin left that can be used to toggle either having the ramp go up  or down
+There is an open pin left that can be used to
+
+- toggle either having the ramp go up  or down
+- Change the clock divider by 2 or 4 perhaps
