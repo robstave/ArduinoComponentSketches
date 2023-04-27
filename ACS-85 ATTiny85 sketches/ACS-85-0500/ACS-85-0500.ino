d7238b178969888ae669364413d5fb50e2865e06
Robert Stave <robstave@yahoo.com>
2023-04-26 23:18:31 -0500

    fixed formattingcmmit

--- ACS-85 ATTiny85 sketches/ACS-85-0500/ACS-85-0500.ino
+++ ACS-85 ATTiny85 sketches/ACS-85-0500/ACS-85-0500.ino
@@ -3,22 +3,22 @@
 /**
  * ACS-85-0500
  * ATTiny85 Atari punk console
- * 
+ *
  * This is a pretty close approximation to the
  * APC.  It consists of two components.
  * The first is an oscillator.
- * when the oscillator changes down, it triggers a 
+ * when the oscillator changes down, it triggers a
  * one shot.  When the one shot is finished, the first oscilllator is
  * free to trigger the one shot again.
- * 
+ *
  * without touching the knobs, its just a squarewave.  As you modify
  * the frequency or one shot length, you get a PWM/Aliasing effect.
- * 
+ *
  * Alas, the resolution as it stands makes an already underwhelming
  * thing into an 8-bit version that is more underwhelming.
- * 
+ *
  * it works..but could be improved.
- 
+
  *
  * External pin 1       = Reset (not used)
  * External pin 2 (PB3) = Osc freq
@@ -33,11 +33,10 @@
  * V 1.0  -  First Version
  *   needs work. Tried to limit analogReads with millis, but that did not work.
  *   maybe there is a conflict w timer 0.
-  
+
  *
  * Rob Stave (Rob the fiddler) ccby 2015
  */
-
 
 //  ATTiny overview
 //                        +-\/-+
@@ -47,9 +46,7 @@
 //                   GND 4|    |5  PB0 nc
 //                        ------
 
-
- 
-//counters for the frequencies
+// counters for the frequencies
 int oscFreq = 55;
 int oscCounter = 0;
 
@@ -61,42 +58,42 @@ bool oneShotActive = false;
 
 bool osc = false;
 
-//avoid a constant call to analog read
+// avoid a constant call to analog read
 long readTimer = 0;
 
-
 // the setup function runs once when you press reset or power the board
-void setup() {
+void setup()
+{
 
-  DDRB = B00000111;  //could use this
- 
+  DDRB = B00000111; // could use this
+
   // initialize timer0
-  noInterrupts();           // disable all interrupts
+  noInterrupts(); // disable all interrupts
 
   TCCR0A = 0;
   TCCR0B = 0;
 
-  TCCR0A |= (1 << WGM01); //Start timer 1 in CTC mode Table 11.5
-  TIMSK |= (1 << OCIE0A); //Enable CTC interrupt see 13.3.6
-  OCR0A = 4; //CTC Compare value
+  TCCR0A |= (1 << WGM01); // Start timer 1 in CTC mode Table 11.5
+  TIMSK |= (1 << OCIE0A); // Enable CTC interrupt see 13.3.6
+  OCR0A = 4;              // CTC Compare value
 
   TCCR0B |= (1 << CS00); // Prescaler =64 Table 11.6
 
-  interrupts();             // enable all interrupts
-
+  interrupts(); // enable all interrupts
 }
 
-ISR(TIMER0_COMPA_vect)          // timer compare interrupt service routine
+ISR(TIMER0_COMPA_vect) // timer compare interrupt service routine
 {
 
-
-readTimer++;
-  //HF code is the same.  Just smaller counters
-  if (oscCounter > oscFreq) {
+  readTimer++;
+  // HF code is the same.  Just smaller counters
+  if (oscCounter > oscFreq)
+  {
     oscCounter = 0;
     osc = !osc;
 
-    if (oneShotReady && (osc == false)) {
+    if (oneShotReady && (osc == false))
+    {
       oneShotReady = false;
       oneShotActive = true;
       oneShotCounter = 0;
@@ -104,32 +101,31 @@ readTimer++;
   }
   oscCounter++;
 
+  if (oneShotActive == true)
+  {
+    PORTB |= (1 << PB2); // Set port High
 
-
-  if ( oneShotActive == true) {
-    PORTB |=  (1 << PB2);           // Set port High
-
-    if ( oneShotCounter > oneShotLength) {
+    if (oneShotCounter > oneShotLength)
+    {
       oneShotCounter = 0;
-      PORTB &= ~(1 << PB2);           // Set port Low
+      PORTB &= ~(1 << PB2); // Set port Low
       oneShotReady = true;
       oneShotActive = false;
     }
     oneShotCounter++;
   }
-
 }
 
-void loop() {
+void loop()
+{
 
+  if (readTimer > 500)
+  {
 
- if (readTimer > 500) {
-    
-  int osc1_t = analogRead(A2);
-  oscFreq = map(osc1_t, 0, 1023, 40,  200);
-  int oneShotDuration_t = analogRead(A3);
-  oneShotLength = map(oneShotDuration_t, 0, 1023, 20,  200);
-readTimer = 0;
- }
-
+    int osc1_t = analogRead(A2);
+    oscFreq = map(osc1_t, 0, 1023, 40, 200);
+    int oneShotDuration_t = analogRead(A3);
+    oneShotLength = map(oneShotDuration_t, 0, 1023, 20, 200);
+    readTimer = 0;
+  }
 }
